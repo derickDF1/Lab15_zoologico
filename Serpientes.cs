5a9b1cbe6a68c31738ea3c4b1de46a73eb71fe9f
derickcux2023 <derickcux2023@LAB4-20>
2024-05-20 09:16:11 -0600

    Clase serpiente corregida, formulario serpiente, editar, eliminar.

--- Serpientes.cs
+++ Serpientes.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 
 namespace Lab15_zoologico
 {
-    public class Serpientes
+    public class Serpientes : Animal
     {
         public bool Venenosa { get; set; }
         public string Longitud { get; set; }
